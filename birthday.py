12235d38858cd44f7868ca137e8171ded1c2a192
N0ah580 <145355575+N0ah580@users.noreply.github.com>
2023-11-29 08:28:45 -0500

    fu

--- birthday.py
+++ birthday.py
@@ -1,11 +1,9 @@
 # Programmer: Noah
 # Description: Birthday
 
-name = input ("what is your name? ")
-
 # Function to print lyrics to happy birthday with given name
 def happy_birthday (name):
     print ("Happy birthday to you")
     print ("Happy birthday to you")
-    print (f"Happy birthday dear {name}")
+    print (f"Happy birthday, dear {name}")
     print ("Happy birthday to you")
--- cone.py
+++ cone.py
@@ -8,11 +8,11 @@ from math import pi
 def get_volume (radius, height):
     step_1 = pi * radius ** 2 * height
     answer = step_1 / 3
-    return (f"{answer}")
+    return answer
 
 def get_surface_area (radius, height):
     part_1 = pi * radius ** 2
     slant = hypot (radius, height)
     part_2 = pi * radius * slant
     answer_sa = part_1 + part_2
-    return (f"{answer_sa}")
+    return answer_sa
