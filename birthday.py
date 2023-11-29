8098dc98aa45cdfc7914cc31473b4bc491531afe
N0ah580 <145355575+N0ah580@users.noreply.github.com>
2023-11-29 08:32:40 -0500

    fuc

--- birthday.py
+++ birthday.py
@@ -2,7 +2,7 @@
 # Description: Birthday
 
 # Function to print lyrics to happy birthday with given name
-def happy_birthday (name):
+def sing_happy_birthday (name):
     print ("Happy birthday to you")
     print ("Happy birthday to you")
     print (f"Happy birthday, dear {name}")
